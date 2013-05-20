4d43cdbae099288bf96bfc1d25318a86acb2be35
Yuntao Jiang <jiangyuntao@gmail.com>
2013-05-20 17:37:55 +0800

    一些逻辑代码

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\Slim(array(
     'model' => 'development',
+    'view' => new \Slim\Extras\Views\Twig(),
     'templates.path' => './templates',
     'cookies.lifetime' => '3 years',
     'cookies.secret_key' => 'laiyitui',
@@ -10,34 +11,87 @@ $app = new \Slim\Slim(array(
     'cookies.cipher_mode' => MCRYPT_MODE_CBC,
 ));
 
+class R extends RedBean_Facade {}
+R::setup('mysql:host=localhost;dbname=laiyitui_com', 'root', '');
+
 // 首页
 $app->get('/', function() use ($app) {
     $data = array(
-        'url_explore_hot' => $app->urlFor('explore', array('way' => 'hot', 'page' => 1))
+        'url' => $app->urlFor('explore')
     );
     $app->render('index.html', $data);
 });
 
 // 图片浏览
-$app->get('/explore/:way/:page', function($way, $page) use ($app) {
-    var_dump($way);
-    var_dump($page);
-    $app->render('index.html');
+$app->get('/explore(/:way(/:page))', function($way = 'latest', $page = 1) use ($app) {
+    $data = array(
+        'latest_url' => $app->urlFor('explore', array('way' => 'latest', 'page' => 1)),
+        'hot_url' => $app->urlFor('explore', array('way' => 'hot', 'page' => 1)),
+        'controversial_url' => $app->urlFor('explore', array('way' => 'controversial', 'page' => 1)),
+        'current' => $way,
+        'auth' => auth($app)
+    );
+    $app->render('explore.html', $data);
 })->name('explore');
 
 // 查看图片
 $app->get('/view/:id', function($id) use ($app) {
-});
+})->name('view');
 
 // 留言
 $app->post('/comment/:id', function($id) use ($app) {
-});
+})->name('comment');
 
-// 上传页面
-$app->get('/upload', function() use ($app) {
-});
-// 上传处理
-$app->post('/upload', function() use ($app) {
-});
+// 上传
+$app->map('/upload', function() use ($app) {
+    $app->render('upload.html');
+})->via('GET', 'POST');
+
+// 登录
+$app->map('/signin', function() use ($app) {
+    $app->render('signin.html');
+})->via('GET', 'POST');
+
+// 注册
+$app->map('/signup', function() use ($app) {
+    if ($app->request()->isGet()) {
+        $app->render('signup.html');
+    } elseif ($app->request()->isPost()) {
+        $user = R::dispense('user');
+        $user->username = $_POST['username'];
+        $user->email = $_POST['email'];
+        $user->password = md5($_POST['password']);
+        $user->created_at = $user->updated_at = time();
+        if ($id = R::store($user)) {
+            $auth = array(
+                'id' => $user->id,
+                'username' => $user->username,
+                'email' => $user->email
+            );
+            signin($app, $auth);
+            $app->redirect('/explore');
+        }
+    }
+})->via('GET', 'POST');
+
+// =================================
+// some hepler functions
+// =================================
+function dump($var = '') {
+    ob_start();
+    var_dump($var);
+    $output = ob_get_clean();
+    echo '<pre>';
+    echo htmlspecialchars($output);
+    echo '</pre>';
+}
+
+function signin($app = null, $auth = array()) {
+    return $app->setEncryptedCookie('auth', serialize($auth));
+}
+
+function auth($app) {
+    return unserialize($app->getEncryptedCookie('auth'));
+}
 
 $app->run();
