4689c2652eec5583b3ffad59ef6b0d2520f01893
Yuntao Jiang <jiangyuntao@gmail.com>
2013-05-20 17:37:45 +0800

    数据库添加 user 表

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@
 -- Server version                :5.5.16 - MySQL Community Server (GPL)
 -- Server OS                     :Win32
 -- HeidiSQL 版本                   :7.0.0.4246
--- Created                       :2013-05-17 16:32:34
+-- Created                       :2013-05-20 17:32:18
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -49,6 +49,27 @@ CREATE TABLE IF NOT EXISTS `picture` (
 DELETE FROM `picture`;
 /*!40000 ALTER TABLE `picture` DISABLE KEYS */;
 /*!40000 ALTER TABLE `picture` ENABLE KEYS */;
+
+
+-- Dumping structure for table laiyitui_com.user
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `created_at` int(10) DEFAULT NULL,
+  `updated_at` int(10) DEFAULT NULL,
+  `deleted_at` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table laiyitui_com.user: 0 rows
+DELETE FROM `user`;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(1, 'admin', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', 1369042241, 1369042241, NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
